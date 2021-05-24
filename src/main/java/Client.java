946f22b9a239a139e14d3d2a1153717d6582fbb7
cezarybednarz <cezary.bednarz@gmail.com>
2021-05-25 00:50:42 +0200

    Add Maven dependencies

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,5 +1,18 @@
+import com.binance.api.client.BinanceApiClientFactory;
+import com.binance.api.client.BinanceApiRestClient;
+import com.binance.api.client.domain.market.TickerStatistics;
+
 public class Client {
     public static void main(String[] args) {
+        BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance("API-KEY", "SECRET");
+        BinanceApiRestClient client = factory.newRestClient();
+
         System.out.println("Hello world");
+
+        long serverTime = client.getServerTime();
+        System.out.println(serverTime);
+
+        TickerStatistics tickerStatistics = client.get24HrPriceStatistics("NEOETH");
+        System.out.println(tickerStatistics.getLastPrice());
     }
 }
